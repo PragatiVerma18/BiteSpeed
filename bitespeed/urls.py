042b7224d02192cbcc7eed8eb1fa9e75f50cd672
PragatiVerma18 <itispragativerma@gmail.com>
2025-02-11 01:00:37 +0530

    Add swagger and redoc documentation

--- bitespeed/urls.py
+++ bitespeed/urls.py
@@ -18,9 +18,35 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from api import urls as api_urls
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="BiteSpeed API",
+        default_version="v1",
+        description="BiteSpeed Identity Reconciliation API",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(api_urls)),
+    # Documentation
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
